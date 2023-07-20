f3d004ab3dfc8651c8a97fa9b44a9bc2a87b0648
riyas <riyasbatechnology@gmail.com>
2023-07-20 16:42:31 +0530

    a

--- lib/screens/members/bus/bus_details.dart
+++ lib/screens/members/bus/bus_details.dart
@@ -1,5 +1,6 @@
 import 'package:custom_clippers/custom_clippers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../../../constands/constands.dart';
